be66f67027b9a6072fdf2830e918224bbe500cba
Jakob McPherson <squaredcubed64@gmail.com>
2024-01-05 07:55:46 -0600

    Move drawing functions to Utils

--- src/BinarySearchTree.ts
+++ src/BinarySearchTree.ts
@@ -1,5 +1,4 @@
-import DisplayNode from './DisplayNode'
-import DataNode from './DataNode'
+import type DataNode from './DataNode'
 import type Tree from './Tree'
 import {
   ROOT_TARGET_X,
@@ -32,46 +31,7 @@ import {
   FIND_DESCRIPTIONS,
   ArrowDirection
 } from './Constants'
-
-// For debugging
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-function toString (node: DataNode | null, depth: number = 0): string {
-  if (node == null) {
-    return ''
-  }
-  let out = '\t'.repeat(depth) + node.displayNode.value.toString() + '\n'
-  out += toString(node.left, depth + 1)
-  out += toString(node.right, depth + 1)
-  return out
-}
-
-function drawArrow (fromX: number, fromY: number, toX: number, toY: number, context: CanvasRenderingContext2D): void {
-  const dx = toX - fromX
-  const dy = toY - fromY
-  const angle = Math.atan2(dy, dx)
-
-  context.beginPath()
-  context.lineWidth = ARROW_LINE_WIDTH
-  context.moveTo(fromX, fromY)
-  context.lineTo(toX, toY)
-  context.lineTo(toX - ARROW_HEAD_LENGTH * Math.cos(angle - ARROW_HEAD_ANGLE), toY - ARROW_HEAD_LENGTH * Math.sin(angle - ARROW_HEAD_ANGLE))
-  context.moveTo(toX, toY)
-  context.lineTo(toX - ARROW_HEAD_LENGTH * Math.cos(angle + ARROW_HEAD_ANGLE), toY - ARROW_HEAD_LENGTH * Math.sin(angle + ARROW_HEAD_ANGLE))
-  context.stroke()
-}
-
-function drawArrowFromNodeToNode (fromNode: DataNode, toNode: DataNode, context: CanvasRenderingContext2D): void {
-  const dx = toNode.displayNode.x - fromNode.displayNode.x
-  const dy = toNode.displayNode.y - fromNode.displayNode.y
-  const dist = Math.sqrt(dx * dx + dy * dy)
-  const xOffsetFromCenter = dx * toNode.displayNode.currentRadius / dist
-  const yOffsetFromCenter = dy * toNode.displayNode.currentRadius / dist
-  drawArrow(fromNode.displayNode.x, fromNode.displayNode.y, toNode.displayNode.x - xOffsetFromCenter, toNode.displayNode.y - yOffsetFromCenter, context)
-}
-
-function makeDataNode (targetX: number, targetY: number, value: number): DataNode {
-  return new DataNode(new DisplayNode(targetX, targetY, FILL_COLOR, STROKE_COLOR, value))
-}
+import { drawArrowFromNodeToNode, makeDataNode } from './Utils'
 
 // Used to implement animations
 interface DelayedFunctionCall {
--- src/Utils.ts
+++ src/Utils.ts
@@ -1,3 +1,7 @@
+import { ARROW_HEAD_ANGLE, ARROW_HEAD_LENGTH, ARROW_LINE_WIDTH, FILL_COLOR, STROKE_COLOR } from './Constants'
+import DataNode from './DataNode'
+import DisplayNode from './DisplayNode'
+
 // Curves that start at 0, 0 and go to 1, 1
 export const motionCurve = (progress: number): number => {
   if (progress < 0 || progress > 1) {
@@ -22,4 +26,32 @@ export const radiusShrinkingCurve = (progress: number): number => {
 export const resizeCanvas = (canvas: HTMLCanvasElement): void => {
   canvas.width = window.innerWidth
   canvas.height = window.innerHeight
+}
+
+const drawArrow = (fromX: number, fromY: number, toX: number, toY: number, context: CanvasRenderingContext2D): void => {
+  const dx = toX - fromX
+  const dy = toY - fromY
+  const angle = Math.atan2(dy, dx)
+
+  context.beginPath()
+  context.lineWidth = ARROW_LINE_WIDTH
+  context.moveTo(fromX, fromY)
+  context.lineTo(toX, toY)
+  context.lineTo(toX - ARROW_HEAD_LENGTH * Math.cos(angle - ARROW_HEAD_ANGLE), toY - ARROW_HEAD_LENGTH * Math.sin(angle - ARROW_HEAD_ANGLE))
+  context.moveTo(toX, toY)
+  context.lineTo(toX - ARROW_HEAD_LENGTH * Math.cos(angle + ARROW_HEAD_ANGLE), toY - ARROW_HEAD_LENGTH * Math.sin(angle + ARROW_HEAD_ANGLE))
+  context.stroke()
+}
+
+export const drawArrowFromNodeToNode = (fromNode: DataNode, toNode: DataNode, context: CanvasRenderingContext2D): void => {
+  const dx = toNode.displayNode.x - fromNode.displayNode.x
+  const dy = toNode.displayNode.y - fromNode.displayNode.y
+  const dist = Math.sqrt(dx * dx + dy * dy)
+  const xOffsetFromCenter = dx * toNode.displayNode.currentRadius / dist
+  const yOffsetFromCenter = dy * toNode.displayNode.currentRadius / dist
+  drawArrow(fromNode.displayNode.x, fromNode.displayNode.y, toNode.displayNode.x - xOffsetFromCenter, toNode.displayNode.y - yOffsetFromCenter, context)
+}
+
+export const makeDataNode = (targetX: number, targetY: number, value: number): DataNode => {
+  return new DataNode(new DisplayNode(targetX, targetY, FILL_COLOR, STROKE_COLOR, value))
 }
